048c37d3ec14744caf1ea112bde2a47bfac2038a
StringsLi <lixin4009@163.com>
2020-06-16 18:21:50 +0800

    no message

---
+++ src/main/scala/com/strings/leetcode/tree/Problem_297_Codec.scala
@@ -0,0 +1,19 @@
+package com.strings.leetcode.tree
+
+class Problem_297_Codec {
+
+
+  // Encodes a list of strings to a single string.
+  def serialize(root: TreeNode): String = {
+
+
+    null
+  }
+
+  // Decodes a single string to a list of strings.
+  def deserialize(s: String): TreeNode = {
+    null
+    
+  }
+
+}
