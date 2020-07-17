4251503ac884ceac6df507c4e34c3d1a01114c90
StringsLi <lixin4009@163.com>
2020-07-17 11:09:47 +0800

    7.17

--- src/main/java/leetcode/list/Problem_142_detectCycle.java
+++ src/main/java/leetcode/list/Problem_142_detectCycle.java
@@ -23,6 +23,28 @@ public class Problem_142_detectCycle {
         return null;
     }
 
+    public ListNode detectCycle2(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast){
+                break;
+            }
+        }
+        if(fast == null || fast.next == null){
+            return null;
+        }
+        slow = head;
+        while(slow != fast){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+
+    }
+
     public static void main(String[] args) {
         ListNode node = new ListNode(1);
         node.next = new ListNode(2);
